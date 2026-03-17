afff4d6ffcc29ccd4818c6d6dc84889c5f95e42c
Aditya <adityabhadauria63@gmail.com>
2026-03-17 10:15:21 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,9 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions=
 {
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173',
+      "https://talent-trellis-fe.lovable.app"
+    ],
     credentials:true,
 }
 app.use(cors(corsOptions));
