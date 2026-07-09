4ffbf71cf0576610b3738c254af4cfb33dfe91d0
Aditya <adityabhadauria63@gmail.com>
2026-07-09 16:22:50 +0530

    use gmail smtp port 587

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -47,11 +47,25 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// const transporter = nodemailer.createTransport({
+//   // service: "gmail",
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+//   connectionTimeout: 10000,
+//   greetingTimeout: 10000,
+//   socketTimeout: 10000,
+// });
+
 const transporter = nodemailer.createTransport({
-  // service: "gmail",
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
