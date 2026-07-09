b74af3b535af3041ecd0eb60f44c29a357d8c8b8
Aditya <adityabhadauria63@gmail.com>
2026-07-09 19:18:12 +0530

    use gmail smtp port 587

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -11,6 +11,8 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false, // true for 465, false for other ports
+  requireTLS: true, // Use TLS
+  family: 4, // Use IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
