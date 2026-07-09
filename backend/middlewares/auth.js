9a7ef9ae47cff4954a04ab10c2a8a353af5dc1df
Aditya <adityabhadauria63@gmail.com>
2026-07-09 14:31:43 +0530

    support bearer token auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,7 +4,10 @@ const auth=async (req,res,next)=>
 {
     try
     {
-        const token = req.cookies.token;
+        // const token = req.cookies.token;
+        const token =
+  req.cookies?.token ||
+  req.headers.authorization?.split(" ")[1];
         if(!token)
         {
             return res.status(401).json(
